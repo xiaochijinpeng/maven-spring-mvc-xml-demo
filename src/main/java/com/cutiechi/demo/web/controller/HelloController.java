01d83c55a88411a5b40b2f157712c9d56db33d7d
cutiechi <chi@cutie.ooo>
2018-09-13 23:11:53 +0800

    feat(api): add hello index page api

--- src/main/java/com/cutiechi/demo/web/controller/HelloController.java
+++ src/main/java/com/cutiechi/demo/web/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.cutiechi.demo.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
+    @GetMapping("")
+    public String index () {
+        return "hello/index";
+    }
 }
