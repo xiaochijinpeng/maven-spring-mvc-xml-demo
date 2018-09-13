45010cff1261b59f5b5b2bad4468ce8978b24fe3
cutiechi <chi@cutie.ooo>
2018-09-13 23:07:38 +0800

    feat(controller): add hello controller

---
+++ src/main/java/com/cutiechi/demo/web/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.cutiechi.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+}
